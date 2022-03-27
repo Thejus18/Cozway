536333cfc16fa94bda2fe6a7faec2ec12e597074
Thejus Mathew <thejusmathew4@gmail.com>
2022-03-27 13:56:26 +0530

    After Order Functionalities

--- orders/admin.py
+++ orders/admin.py
@@ -3,10 +3,22 @@ from.models import Payment, Order, OrderProduct
 
 # Register your models here.
 
+#tabular Inline-- 
+class OrderProductInline(admin.TabularInline):      #to apend the orderProduct table below the orders
+    model=OrderProduct
+    readonly_fields = ('payment', 'user', 'product', 'qunatity', 'product_price', 'ordered')
+    extra = 0
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['order_number','full_name','phone','email','city','order_total','tax','status','ip','is_ordered','created_at']
+    list_filter= ['status', 'is_ordered']
+    search_fileds = ['order_number','first_name','last_name','phone','email']
+    list_per_page =20
+    inlines = [OrderProductInline]      #to apend the orderProduct table below the orders
 
 
 
 
 admin.site.register(Payment)
-admin.site.register(Order)
+admin.site.register(Order,OrderAdmin)
 admin.site.register(OrderProduct)
--- orders/views.py
+++ orders/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render,redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from carts.models import CartItem
 from .forms import OrderForm
-from.models import Order, Payment
+from store.models import Product
+from.models import Order, Payment,OrderProduct 
 import datetime
 import json
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
 
 def payments(request):
        #for saving the payment details inside the database
@@ -18,13 +21,63 @@ def payments(request):
          amount_paid =  order.order_total,
          status = body['status'],
     )
-    payment.save()          #for saving the payment details inside the database
+    payment.save()          #for saving the payment details inside the database(admin panel)
     
     order.payment = payment
     order.is_ordered = True
-    order.save()           #for saving the payment details inside the database
-    return render(request, 'orders/payments.html')
-# Create your views here.
+    order.save()           #for saving the payment details inside the database(admin panel)
+   
+   
+   #Move the cart items to Order Product table
+    cart_items = CartItem.objects.filter(user=request.user)
+    
+    for item in cart_items:
+        orderproduct = OrderProduct()
+        orderproduct.order_id= order.id
+        orderproduct.payment = payment
+        orderproduct.user_id = request.user.id
+        orderproduct.product_id = item.product_id 
+        orderproduct.qunatity = item.qunatity
+        orderproduct.product_price = item.product.price
+        orderproduct.ordered = True
+        orderproduct.save()         #for saving the payment details inside the database(admin panel)
+
+        #Move the variations to Order Product table
+        cart_item = CartItem.objects.get(id=item.id)
+        product_variation = cart_item.variations.all()
+        orderproduct = OrderProduct.objects.get(id=orderproduct.id)
+        orderproduct.variations.set(product_variation)
+        orderproduct.save()
+
+
+        #Reduce the quantity pf the sold products
+        product = Product.objects.get(id=item.product_id)
+        product.stock -= item.qunatity
+        product.save()
+
+    #clear cart
+    CartItem.objects.filter(user=request.user).delete()
+
+    #send order recieved mail to customer
+    mail_subject = 'Thank you for your order!!'
+    message = render_to_string('orders/order_recived_email.html',{
+         'user': request.user,  
+         'order': order,       
+    })
+    to_email = request.user.email
+    send_email = EmailMessage(mail_subject,message,to=[to_email])
+    send_email.send()
+
+    # Send order number and transaction id back to sendData method via JsonResponse
+    data ={
+        'order_number' : order.order_number,
+        'transID' : payment.payment_id,
+    }
+    return JsonResponse(data)
+   
+   
+    
+
 
 
 
@@ -90,3 +143,33 @@ def place_order(request, total=0, qunatity=0):
            
     else:
         return redirect('checkout')
+
+def order_complete(request):
+    order_number = request.GET.get('order_number')
+    transID = request.GET.get('payment_id')
+
+    try:
+        order = Order.objects.get(order_number=order_number, is_ordered=True)
+        ordered_products = OrderProduct.objects.filter(order_id=order.id)
+
+        subtotal = 0
+        for i in ordered_products:
+            subtotal += i.product_price * i.qunatity
+
+        payment = Payment.objects.get(payment_id=transID)       #taking the payment from the database
+
+        context = {
+            'order': order,
+            'ordered_products': ordered_products,
+            'order_number': order.order_number,
+            'transID': payment.payment_id,
+            'payment': payment,
+            'subtotal': subtotal,
+        }
+        return render(request, 'orders/order_complete.html', context)
+    except (Payment.DoesNotExist, Order.DoesNotExist):
+        return redirect('home')
+
+
+
+  
