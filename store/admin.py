15295cec1e241461aca3f4ab5e69cee4d27ff4e0
Thejus Mathew <thejusmathew4@gmail.com>
2022-04-04 22:11:03 +0530

    Product Gallery and rating stars

--- store/admin.py
+++ store/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
-from .models import Product, Variation,ReviewRating
+from .models import Product, Variation,ReviewRating,ProductGallery
+import admin_thumbnails
 
+@admin_thumbnails.thumbnail('image') #For using the admin_thumbnail
 # Register your models here.
+
+class ProductGalleryInline(admin.TabularInline):
+    model = ProductGallery
+    extra = 1
+
 class productAdmin(admin.ModelAdmin):
     
     prepopulated_fields = {'slug' : ('product_name',)}     #for prepoplating slug
     list_display = ('product_name', 'price','stock','category','modified_date','is_available')
+    inlines=[ProductGalleryInline]
 
 class VariationAdmins(admin.ModelAdmin):
     list_display =('product','variation_category','variation_value','is_active' )
@@ -16,3 +24,4 @@ class VariationAdmins(admin.ModelAdmin):
 admin.site.register(Product, productAdmin)
 admin.site.register(Variation,VariationAdmins )
 admin.site.register(ReviewRating)
+admin.site.register(ProductGallery)
--- store/models.py
+++ store/models.py
@@ -78,3 +78,17 @@ class ReviewRating(models.Model):
 
     def __str__(self):                                          #string represenatation of the Model
         return self. subject
+
+class ProductGallery(models.Model):
+    product = models.ForeignKey(Product,default=None, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='store/products/',max_length=255)
+
+
+    def __str__(self):
+        return self.product.product_name
+
+    class Meta:     #This class is created because django autimatcially add an 's' to every models to make it plural, so for removing that 's' we are using this class
+        verbose_name = 'productgallery'
+        verbose_name_plural = 'product gallery'    
+
+
